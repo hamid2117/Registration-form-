690ba534ade326d78b61998b15e9e9cbd453e03b
hamid2117 <saim008865@gmail.com>
2021-09-18 15:15:00 +0500

    Included type in the model of user

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -12,7 +12,7 @@ const userSchema = mongoose.Schema(
     pin: { type: String, required: true },
     img: { type: String },
     expireDate: { type: String },
-    isAdmin: { type: Boolean, required: true, default: false },
+    type: { type: String, required: true, default: 'user' },
   },
   { timestamps: true }
 )
